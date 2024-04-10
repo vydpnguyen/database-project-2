89d41631e521be3d754de3ddc9b57ba318d1170d
Merelifluous <110203961+tnanhx@users.noreply.github.com>
2024-04-10 12:19:33 -0700

    summary blocks

--- interface_elephantsql/author_data.py
+++ interface_elephantsql/author_data.py
@@ -1,2 +1,8 @@
-authors_to_insert = [(1, 'Agatha', 'Christie'), (2, 'Marissa', 'Meyers'), (3,'Timothy', 'Goyle')]
-
+authors_to_insert = [
+(1, 'Agatha', 'Christie'), 
+(2, 'Marissa', 'Meyers'), 
+(3,'Timothy', 'Goyle'),
+(4, 'Samantha', 'Keller'),
+(5, 'Julie', 'Roberts'),
+(6, 'John', 'Wick'),
+(7, 'Dwight', 'Fairfield')]
