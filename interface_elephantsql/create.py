d4d4afd04b03aee1900c528a299321fb7e4eebde
Merelifluous <110203961+tnanhx@users.noreply.github.com>
2024-04-10 11:52:38 -0700

    Change data type

--- interface_elephantsql/create.py
+++ interface_elephantsql/create.py
@@ -10,13 +10,13 @@ DATABASE_URLS = [
 # SQL command to create the "author" and "book" table
 create_table_command = sql.SQL("""
 CREATE TABLE IF NOT EXISTS author (
-    author_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    author_id INTEGER PRIMARY KEY,
     author_fn VARCHAR(255) NOT NULL,
     author_ln VARCHAR(255) NOT NULL
 );                        
                                
 CREATE TABLE IF NOT EXISTS book (
-    book_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    book_id INTEGER PRIMARY KEY,
     book_name VARCHAR(255) NOT NULL,
     author_id INTEGER NOT NULL,
     book_summary TEXT NOT NULL
@@ -44,6 +44,8 @@ def create_table_in_database(db_url):
         # Close communication with the database
         if cur: cur.close()
         if conn: conn.close()
+
+        
 
 # Iterate over each database URL and create the table
 for db_url in DATABASE_URLS:
---
+++ interface_elephantsql/slow.py
@@ -0,0 +1,7 @@
+import psycopg2
+from psycopg2 import sql
+
+DATABASE_URLS = [
+    "postgres://lddwokth:mJvHU-ZoB_5_dvqP_43CM_YWeDIWv9Jf@isilo.db.elephantsql.com/lddwokth", #AuthorDB
+    "postgres://hdxwvmpg:GcZMUqM7HoeEYP9cvmxGLR-ufqlqC1Ss@bubble.db.elephantsql.com/hdxwvmpg" #BookDB
+]
